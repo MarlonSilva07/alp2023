6c4f55bd8ac966a64d660a7f9227889f03810f06
MarlonSilva07 <130609680+MarlonSilva07@users.noreply.github.com>
2023-05-01 21:51:55 -0300

    Add files via upload

---
+++ questão3.js
@@ -0,0 +1,4 @@
+let sal_minimo = Number(prompt("Informe o valor do salário mínimo"));
+let sal_usuario = Number(prompt("Informe o valor do seu salário"));
+let quant_salario = sal_usuario / sal_minimo;
+document.write(`Você ganha ${quant_salario.toFixed(1)} salários mínimos.`);
