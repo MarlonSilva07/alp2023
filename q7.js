cd85734a16b5dab5b190387e9292b7e20b27e3e5
MarlonSilva07 <130609680+MarlonSilva07@users.noreply.github.com>
2023-05-25 08:06:51 -0300

    Update q7.js

--- q7.js
+++ q7.js
@@ -1,19 +1,19 @@
 let peso=Number(prompt("Digite seu peso"))
 let altura=Number(prompt("Digite sua Altura"))
-let imc=peso/altura**2
+let imc=peso/(altura/100)**2
 if(imc<18,5){
     document.write("Você está abaixo do peso")
 }
-else if(imc>=18,5 && imc<24,9){
+else if(imc>=18,5 && imc<=24,9){
     document.write("Você está com peso normal")
 }
-else if(imc>=25 && imc<29,9){
+else if(imc>=25 && imc<=29,9){
     document.write("Você está Acima do peso (sobrepeso)")
 }
-else if(imc>=30 && imc<34,9){
+else if(imc>=30 && imc<=34,9){
     document.write("Você está com Obesidade I")
 }
-else if(imc>=35 && imc<39,9){
+else if(imc>=35 && imc<=39,9){
     document.write("Você está com Obesidade II")
 }
 else{
